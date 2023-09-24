4ddc1a85be5ee9c81bb83f3b1e14f810aaa884c7
Suman Bogati <sumanbogati@gmail.com>
2023-09-24 17:32:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const name = "This is very simple text";
-let message = "This should not be replaced and the error should be shown by the linter"
+const message = "This should not be replaced and the error should be shown by the linter"
 console.log(name);
 console.log(message);
